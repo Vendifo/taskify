14bc2a065c380d64e4b84454d35722728c192241
@Vendifo <maloy5916@inbox.ru>
2025-07-10 07:52:28 +0900

    feat: setup TailwindCSS, clean boilerplate, add Vue Router

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="max-w-md mx-auto mt-10 p-4 bg-white rounded shadow">
+    <h1 class="text-2xl text-center font-bold mb-4">Taskify — To-Do App</h1>
+    <!-- Здесь будем делать форму и список -->
+  </div>
+</template>
+
+<script setup>
+</script>
