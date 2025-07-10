7819205b749deeb781be653d3fed0b4d904f8fb5
@Vendifo <maloy5916@inbox.ru>
2025-07-11 05:55:02 +0900

    feat: add task filters (all, active, done) with filtering logic and UI

---
+++ src/components/TaskFilter.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="flex gap-4 mb-4">
+		<button
+			v-for="option in options"
+			:key="option.value"
+			@click="$emit('update:modelValue', option.value)"
+			:class="[
+				'px-4 py-2 rounded transition-colors duration-300',
+				modelValue === option.value
+					? 'bg-blue-600 text-white shadow-md'
+					: 'bg-gray-200 text-gray-700 hover:bg-blue-100',
+			]"
+		> {{ option.label }}</button>
+	</div>
+</template>
+
+<script setup>
+defineProps({
+	modelValue: String,
+})
+
+const options = [
+	{label: 'Все', value: 'all'},
+	{label: 'Активные', value: 'active'},
+	{label: 'Выполненые', value: 'done'},
+]
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
 	<div class="p-8 max-w-2x1 mx-auto">
 		<h1 class="text-3x1 font-bold mb-4">Taskufy</h1>
+		<TaskFilter v-model="filter" />
+
 		<TodoInput @add-task="addTask" />
 		<TodoList
-			:tasks="tasks"
+			:tasks="filteredTasks"
 			@toggle-done="toggleDone"
 			@delete-task="deleteTask"
 		/>
@@ -11,13 +13,25 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import TodoInput from "../components/TodoInput.vue";
 import TodoList from "../components/TodoList.vue";
+import TaskFilter from "../components/TaskFilter.vue";
 import { loadTasks, saveTasks } from "../utils/storage";
 
 const tasks = ref(loadTasks());
 
+const filter = ref("all");
+
+const filteredTasks = computed(() => {
+	if (filter.value === "active") {
+		return tasks.value.filter((task) => !task.done);
+	}
+	if (filter.value === "done") {
+		return tasks.value.filter((task) => task.done);
+	}
+	return tasks.value;
+});
 
 function addTask(taskText) {
 	if (taskText.trim()) {
